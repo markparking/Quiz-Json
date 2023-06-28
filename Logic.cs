c4260025c1642c8785a4c100355713689c611729
Mark Parking <mp@praktikcenter.lan>
2023-06-28 08:41:02 +0200

    Yo

---
+++ Logic.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JSONQuiz
+{
+    public class Quiz
+    {
+        public string Question { get; set; }
+        public List<string> Answers { get; set; }
+        public int Correct { get; set; }
+        public string Description { get; set; }
+    }
+
+    internal class Logic
+    {
+        public static string fileName = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory() + "\\quiz.json"));
+        
+        public static List<Quiz> GetQuiz()
+        {
+            return JsonConvert.DeserializeObject<List<Quiz>>(fileName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Quiz> list = Logic.GetQuiz();
+
+            foreach (Quiz quiz in list)
+            {
+                Console.WriteLine(quiz.Question);
+                for (int i = 0; i < quiz.Answers.Count; i++)
+                {
+                    Console.WriteLine($"[{i}] " + quiz.Answers[i]);
+                }
+
+                int choice;
+
+                do
+                {
+                    Console.Write("Enter your choice: [0] [1] [2] ");
+                    if (!int.TryParse(Console.ReadLine(), out choice))
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+
+                    if (choice < 0 || choice > quiz.Answers.Count)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+
+                    break;
+
+                } while (true);
+                string result = (quiz.Answers[quiz.Correct] == quiz.Answers[choice]) ? "Correct answer!" : "Incorrect answer!";
+                
+                Console.WriteLine($"\n{result}\n{quiz.Description}\n");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
